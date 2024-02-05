029a6fc22813b43c34cad620f2372546ce49a440
hchuzhong <hchuzhong@163.com>
2024-02-05 13:56:01 +0800

    handle errors

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -3,14 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const createBlogSchema = z.object({
-    title: z.string().min(1).max(255),
-    content: z.string().min(1),
+    title: z.string().min(1, 'Title is required').max(255),
+    content: z.string().min(1, 'Content is required'),
 });
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
     const validation = createBlogSchema.safeParse(body);
-    if (!validation.success) return NextResponse.json(validation.error.errors, {status: 401});
+    if (!validation.success) return NextResponse.json(validation.error.format(), {status: 401});
     const user = await prisma.post.create({
         data: {
             ...body,
--- app/blog/new/page.tsx
+++ app/blog/new/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { Button, TextField } from "@radix-ui/themes";
-import React from 'react';
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import React, { useState } from 'react';
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
@@ -16,25 +16,33 @@ interface BlogForm {
 export default function NewBlog() {
     const { register, control, handleSubmit } = useForm<BlogForm>();
     const router = useRouter();
+    const [error, setError] = useState('');
 
     return (
-        <form className="space-y-5" onSubmit={handleSubmit(async (data) => {
-            try {
-                await axios.post('/api/blog', data);
-                router.push('/blog');
-            } catch (error) {
-                console.log('error:', error);
-            }
-        })}>
-            <TextField.Root>
-                <TextField.Input placeholder="Title" {...register('title')} />
-            </TextField.Root>
-            <Controller
-                name="content"
-                control={control}
-                render={({ field }) => <SimpleMDE placeholder="Content" {...field} />}
-            />
-            <Button>Submit New Blog</Button>
-        </form>
+        <div>
+            {error && <Callout.Root color="red" className="mb-5">
+                <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>}
+            <form className="space-y-5" onSubmit={handleSubmit(async (data) => {
+                console.log('check data:', data);
+                try {
+                    await axios.post('/api/blog', data);
+                    router.push('/blog');
+                } catch (error) {
+                    console.log('error:', error);
+                    setError('An unexpected error occured');
+                }
+            })}>
+                <TextField.Root>
+                    <TextField.Input placeholder="Title" {...register('title')} />
+                </TextField.Root>
+                <Controller
+                    name="content"
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder="Content" {...field} />}
+                />
+                <Button>Submit New Blog</Button>
+            </form>
+        </div>
     )
 }
